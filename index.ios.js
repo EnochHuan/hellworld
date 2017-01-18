9e4252a2864b380894c51f8989a1b9e3c02a5612
Enoch <zhh123sy@163.com>
2017-01-18 07:30:32 -0800

    初始提交
    初始提交

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+        <Text>Hello world11!</Text>,
+        <Text>Hello world2222ß!</Text>
+    );
+  }
+}
+
+// 注意，这里用引号括起来的'HelloWorldApp'必须和你init创建的项目名一致
+AppRegistry.registerComponent('hellworld', () => HelloWorldApp);
